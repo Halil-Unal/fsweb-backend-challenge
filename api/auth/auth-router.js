904c3648c2054469d8436f97fc008e8cb68c8d6d
HALİL ÜNAL <125642299+Halil-Unal@users.noreply.github.com>
2023-06-03 17:05:37 +0300

    GG

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -4,7 +4,7 @@ const { JWT_SECRET } = require("../secrets");
 const jwt = require("jsonwebtoken");
 const bcryptjs = require("bcryptjs");
 const userModel = require("../users/users-model");
-
+jwtCache =[];
 router.post("/register",checkPayload, async(req, res, next) => {
 
   try {
@@ -35,5 +35,27 @@ router.post("/login",checkPayload, usernameVarmi, (req, res, next) => {
     next(error);
   }
 });
+router.get('/logout', (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (token) {
+    try {
+      jwt.verify(token, JWT_SECRET); // Verify the token
+
+      // Token is valid, perform logout action
+      res.clearCookie('token'); // Clear the token cookie
+
+      res.json({
+        message: "Gene bekleriz!...",
+        clearToken: true // Add a flag to indicate clearing the token on the client-side
+      });
+    } catch (error) {
+      next({ status: 400, message: "Token is not valid!..." });
+    }
+  } else {
+    next({ status: 400, message: "Token is not provided!..." });
+  }
+});
+
 
 module.exports = router;
